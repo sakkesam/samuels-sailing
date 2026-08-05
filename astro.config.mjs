b568f86fe8d31bc4a5b8dcd1d02f289802788331
Sakke Samuels <samuels.sakke@gmail.com>
2026-08-05 16:58:43 +0200

    Site finish and first blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,37 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), icon()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Outfit",
+      cssVariable: "--font-outfit",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/outfit-v15-latin-regular.woff2"],
+            weight: 400,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: ["./src/assets/fonts/outfit-v15-latin-700.woff2"],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+  ],
 });
---
+++ src/content/blog/post-1.md
@@ -0,0 +1,30 @@
+---
+title: "Welcome to our blog!"
+description: "Introduction and what you can expect to read in our blog"
+pubDate: 2026-08-05
+heroImage: "../../assets/blog/post-1/sakkechar.jpeg"
+---
+
+# Welcome to our blog / crusing journal!
+
+We are Charlotte and Sakke. We quit our jobs as PE teachers and bought a 40ft sailboat to live on. We're excited to have you join us on our adventures!
+
+We wanted to start this blog so friends, family or other visitors, (Hello to you if you are one 👋) could have a place where they can read about our adventure at sea.
+
+Here's a glimpse of what you can expect from our posts:
+
+## What we'll be sharing
+* **Posts about living on a sailboat:** Posts about our day to day life of living on a sailboat.
+* **Boat maintenance and DIY projects:** Although we're not super handy with fixing the boat (yet). Writing about boat maintenance and diy projects might be a good way for us to learn things and hopefully they might help other people out too!
+* **Galley Stories & Port Guides:** Cooking in a moving kitchen, exploring new ports, and finding hidden anchorages along our route. (Galley is just a fancy boat word for kitchen, you can expect a lot more fancy boat words too!)
+* **And whatever we might feel like writing about!**
+
+If there's anything you would like to read about you can find us on Instagram [@samuelssailing](https://www.instagram.com/samuelssailing/) and send us message!
+
+```text
+Charlotte & Sakke
+15:00 UTC
+Position 37°35'50.272'N 0°58'48.786'E
+```
+
+
