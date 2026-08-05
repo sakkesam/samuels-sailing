dc5b0911a5975d45b78d94d7088e555303048f27
Sakke Samuels <samuels.sakke@gmail.com>
2026-08-05 17:52:22 +0200

    fixed image imports

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-
-
 export const SITE_TITLE = "Samuels Sailing";
-export const SITE_DESCRIPTION = 'Charlotte & Sakke Sailing Blog';
+export const SITE_DESCRIPTION = "Charlotte & Sakke Sailing Blog";
